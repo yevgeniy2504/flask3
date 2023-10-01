16d460f7a84dc44d91607d082b9ed9f83de1f787
Yevgeniy Karabekov <yevgeniy.karabekov@gmail.com>
2023-10-01 23:13:12 +0600

    complete

---
+++ lesson_6/settings.py
@@ -0,0 +1,12 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+
+    # class Config:
+    #     env_file = '.env'
+    #     env_file_encoding = 'utf-8'
+
+
+settings = Settings()
